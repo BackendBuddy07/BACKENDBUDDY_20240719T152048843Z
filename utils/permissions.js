2144d5408e88c4d920baeb3f63f690d19f64de70
BackendBuddy07 <backendbuddy07@gmail.com>
2024-07-19 15:21:20 +0000

    Changes updated

---
+++ utils/permissions.js
@@ -0,0 +1,4 @@
+exports.PERMISSIONS = ["creating", "deleting", "updating"]; 
+exports.ROLES = ["unrestricted2"] 
+exports.UNRESTRICTED2 = ["unrestrictedPermission1", "deleting"]; 
+exports.RESTRICTED_ROLES = []; 
